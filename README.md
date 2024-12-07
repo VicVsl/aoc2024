e2584c45a803132485a77aa7b8b43f47d7468d1d
VicVsl <vic@vansteelant.be>
2024-12-07 14:11:10 +0100

    Day 7

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -111,9 +111,9 @@ Both parts: 845.653ms
 
 ```
 Day 07
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 6.7ms
+Time part 2: 866.674ms
+Both parts: 873.374ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 12/50
-Total time: 870.396ms
+Total stars: 14/50
+Total time: 1743.77ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day07/index.ts
@@ -0,0 +1,87 @@
+import run from 'aocrunner'
+
+const parseInput = (rawInput: string) => rawInput.split('\n').map(line => {
+	const [res, list] = line.split(': ');
+	return {
+		res: Number(res),
+		list: list.split(' ').map(Number)
+	}
+})
+
+const part1 = (rawInput: string) => {
+	const input = parseInput(rawInput)
+
+	return input.reduce<number>((count, { res, list }) => {
+		const permutations = list.reduce<number[]>((acc, val) => {
+			const newAcc: number[] = []
+			acc.forEach(accVal => {
+				if (accVal > res) return
+
+				newAcc.push(accVal + val)
+				newAcc.push(accVal * val)
+			})
+			return newAcc
+		}, [1])
+		if (permutations.includes(res)) count += res
+		return count
+	}, 0)
+}
+
+const part2 = (rawInput: string) => {
+	const input = parseInput(rawInput)
+
+	return input.reduce<number>((count, { res, list }) => {
+		const permutations = list.reduce<number[]>((acc, val) => {
+			const newAcc: number[] = []
+			acc.forEach(accVal => {
+				if (accVal > res) return
+
+				newAcc.push(accVal + val)
+				newAcc.push(accVal * val)
+				newAcc.push(Number(`${accVal}${val}`))
+			})
+			return newAcc
+		}, [1])
+		if (permutations.includes(res)) count += res
+		return count
+	}, 0)
+}
+
+run({
+	part1: {
+		tests: [
+			{
+				input: `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`,
+				expected: 3749,
+			},
+		],
+		solution: part1,
+	},
+	part2: {
+		tests: [
+			{
+				input: `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`,
+				expected: 11387,
+			},
+		],
+		solution: part2,
+	},
+	trimTestInputs: true,
+	onlyTests: false,
+})
